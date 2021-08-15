2ee332c7d842115655f4de6aa34c6e6c1b324d9b
5s <olyguy.aq@gmail.com>
2021-08-15 08:30:04 +0100

    added arithmetic mean and geometric mean

--- geometric_mean.py
+++ geometric_mean.py
@@ -1,27 +1,27 @@
 def accept_values():
-    # global positive_inputs
     positive_values = []
 
-    # positive_inputs
     while True:
-        positive_inputs = int(input())
+        positive_inputs = int(input("Please enter a value or terminate with a negative value: "))
         if positive_inputs >= 0:
             positive_values.append(positive_inputs)
-        # print(positive_values)
 
         elif positive_inputs < 0:
             break
         print(positive_values)
-    return positive_values
+    summation = sum(positive_values)
+    length = len(positive_values)
+    arithmetic_mean = summation / length
+    print("Your arithmetic mean is: ", arithmetic_mean)
 
-    # break
+    product = 1
+    for x in positive_values:
+        product *= x
+    # print(product)
+    geometric_mean = product ** (1 / length)
+    print("Your geometric mean is: ", geometric_mean)
 
 
-def arithmetic_mean():
-    print(accept_values())
-    # arith_mean = sum(positive_values)
-
-
-arithmetic_mean()
+accept_values()
 
 # mean()
